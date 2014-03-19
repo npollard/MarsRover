b90207076a1b7e0e2ec9735cb51ba39d6e58b666
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-03-18 20:05:51 -0700

    Setup.

---
+++ src/test/java/app/RoverTest.java
@@ -0,0 +1,18 @@
+package app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+
+public class RoverTest {
+ 
+	@Test
+	public void testBasic() {
+    assertTrue(true);
+  }   
+ 
+
+}
