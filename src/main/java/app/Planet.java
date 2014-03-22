0ee58e279dc2be240e92c6516412d8a414292560
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-03-22 16:39:38 -0700

    Input validation for Planet and Rover.

--- src/main/java/app/Planet.java
+++ src/main/java/app/Planet.java
@@ -5,6 +5,11 @@ public class Planet {
   int height;
 
   public Planet(int width, int height) {
+    if (width < 1 || height < 1) {
+      System.err.printf("ERROR: invalid planet size.\n");
+      System.exit(1);
+    }
+
     this.width = width;
     this.height = height;
 
--- src/main/java/app/Rover.java
+++ src/main/java/app/Rover.java
@@ -5,6 +5,16 @@ public class Rover {
   char orientation;
 
   public Rover(int x, int y, char orientation) {
+    if (x < 0 || y < 0) {
+      System.err.printf("ERROR: invalid rover start coordinates.\n");
+      System.exit(1);
+    }
+
+    if (orientation != 'N' && orientation != 'S' && orientation != 'E' && orientation != 'W') {
+      System.err.printf("ERROR: invalid rover start orientation.\n");
+      System.exit(1);
+    }
+
     this.x = x;
     this.y = y;
     this.orientation = orientation;
